2f4e5672ad09c344caab1fdb8356aa1d0b727e7c
yangfei <1033327537@qq.com>
2018-01-03 16:08:05 +0800

    Native 远程测试

---
+++ src/main/java/com/self/test/TestNative.java
@@ -0,0 +1,13 @@
+package com.self.test;
+
+public class TestNative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Object obj;
+	}
+	
+	
+	public native Class<?> getClass1();
+
+}

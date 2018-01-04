acac2f1482078673f4019c9fba3d6c424a26db08
yangfei <1033327537@qq.com>
2018-01-04 10:03:34 +0800

    本地提交测试

--- src/main/java/com/self/test/Test.java
+++ src/main/java/com/self/test/Test.java
@@ -1,8 +1,20 @@
 package com.self.test;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("test!");
+		Object o1 = null;
+		System.out.println((BigDecimal)o1);
+		System.out.println((Object)"12.33");
+//		System.out.println(new BigDecimal(12.2f));
+		
+		
+		DecimalFormat decFormat = new DecimalFormat("#.0000");
+		
+		System.out.println( decFormat.format(new BigDecimal("10.00000")));
 	}
 	
 	
